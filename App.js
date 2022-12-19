349d7d91c51f9ef4032405519830b27940325074
Felipe de Menezes <felipemenezes.98@outlook.com>
2022-12-18 22:35:50 -0300

    inclusão de projeto

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar, SafeAreaView } from 'react-native';
+
+import Cesta from './src/pages/Cesta';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
+  );
+}
