a2856e6cb2bc0d6a4f6e64a1a9c175fd839f0a6d
Felipe de Menezes <felipemenezes.98@outlook.com>
2022-12-20 00:03:13 -0300

    inclusão de projeto

--- src/Components/Texto.js
+++ src/Components/Texto.js
@@ -5,7 +5,7 @@ export default function Texto({ children, style }) {
     let estilo = estilos.texto;
 
     if (style?.fontWeight === 'bold') {
-        estilo = estilo.textoNegrito;
+        estilo = estilo.textoNegrito
     }
 
     return <Text style={[style, estilos.texto]}>{children}</Text>
