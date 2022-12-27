76355cf967c5294d2768d8f062252fb8bd7a43a4
Felipe de Menezes Borrego <fmenezes@ccmrp.local>
2022-12-26 21:27:33 -0300

    inclusão do component final do projeto

--- src/pages/Cesta/Index.js
+++ src/pages/Cesta/Index.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ScrollView, FlatList } from "react-native";
 
 import Topo from "./Components/Topo";
 import Detalhes from "./Components/Detalhes";
+import Item from "./Components/Item";
+import Texto from "../../Components/Texto";
 
-export default function Cesta({ topo, detalhes }) {
+export default function Cesta({ topo, detalhes, itens }) {
     return <>
-        <Topo {...topo} />
-        <View style={estilos.cesta}>
-            <Detalhes {...detalhes} />
-        </View>
+        <FlatList
+            data={itens.lista}
+            renderItem={Item}
+            keyExtractor={({ nome }) => nome}
+            ListHeaderComponent={() => {
+                return <>
+                    <Topo {...topo} />
+                    <View style={estilos.cesta}>
+                        <Detalhes {...detalhes} style={estilos.cesta} />
+                        <Texto style={estilos.titulo}>{itens.titulo}</Texto>
+                    </View>
+                </>
+            }}
+        />
     </>
 }
 
@@ -17,5 +29,13 @@ const estilos = StyleSheet.create({
     cesta: {
         paddingVertical: 8,
         paddingHorizontal: 16,
+    },
+    titulo: {
+        color: "#464646",
+        fontWeight: "bold",
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32
     },
 })
